6cd9d89462afffdcf9de6945f9e6ca381415472f
RoshniPal <37709570+RoshniPal@users.noreply.github.com>
2019-05-20 20:56:38 +0530

    Final to-do app

--- todo.sql
+++ todo.sql
@@ -39,22 +39,6 @@ CREATE TABLE `notes` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `notes`
---
-
-INSERT INTO `notes` (`id`, `user_id`, `title`, `description`, `finished`, `created_at`, `updated_at`) VALUES
-(1, 1, 'Dmart', 'item1 , item2', 0, '2019-05-19 11:24:23', '2019-05-19 11:24:23'),
-(2, 1, 'demo', 'demo12345', 0, '2019-05-19 13:13:18', '2019-05-19 13:13:18'),
-(3, 1, 'new entry', 'new qw3h flkrjfn jfewhjkwf', 1, '2019-05-19 13:16:52', '2019-05-19 13:16:52'),
-(4, 1, 'reminder', 'buy cake', 1, '2019-05-19 13:17:40', '2019-05-19 13:17:40'),
-(5, 1, 'check UI', 'see', 0, '2019-05-19 13:20:40', '2019-05-19 13:20:40'),
-(6, 1, 'check id', 'adding this data updated', 0, '2019-05-19 13:30:14', '2019-05-19 22:32:44'),
-(7, 1, 'hi ', 'demo123345', 1, '2019-05-19 13:31:30', '2019-05-19 13:31:30'),
-(8, 1, 'new ', 'this is fun', 1, '2019-05-19 13:33:22', '2019-05-19 13:33:22'),
-(9, 1, 'checking  updated new', 'insertion success updated new', 0, '2019-05-19 13:37:51', '2019-05-19 22:30:54'),
-(10, 7, 'new enrtry', 'this is to remind me about buying cake', 0, '2019-05-19 13:51:26', '2019-05-19 13:51:26');
-
--- --------------------------------------------------------
 
 --
 -- Table structure for table `users`
@@ -69,12 +53,6 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `name`, `email`, `salt`, `password`) VALUES
-(1, 'demo1', 'demo1@gmail.com', '56e00a04af', 'OGNmOGY3Yzg0Y2JmNDk4NTZhZDY3NTZhNTkwZjg3MGI4NzAzYWRjOQ=='),
-(7, 'demouser', 'demouser@gmail.com', 'dcbad6aa95', 'ZmVkN2IwYzhlYjYxOTFkYzQ0ZmY1MzFmYzI2MzM4NWZiMWNjNjM3NA==');
 
 --
 -- Indexes for dumped tables
